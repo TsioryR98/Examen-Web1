f7f49f6c4fb5852bf3d6b5790c842c92cfac958e
Tsiory98 <hei.tsiory.2@gmail.com>
2024-04-20 14:07:10 +0200

    feat: Login page with css animations and JS for the auth'error

---
+++ js/login.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", function(){
+    const email = document.querySelector("#usermail");
+    const passwords = document.querySelector("#passwords");
+    const valide= document.querySelector("#validate");
+    var error = document.querySelector("#error_message")
+    const _login1 = "tsioryR";
+    const _login2 = "azerty01";
+
+    valide.addEventListener('click',() => {
+        if (email.value != _login1 || passwords.value != _login2) {
+            error.style.display = "block";
+        } else {
+            alert("hello")
+        }
+    }
+)
+});
