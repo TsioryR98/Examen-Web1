93118dbfc46c4ef914cd8da41d6287b2198dff24
Tsiory98 <hei.tsiory.2@gmail.com>
2024-05-01 22:39:21 +0200

    feat: edit JS file for login page

--- js/login.js
+++ js/login.js
@@ -1,19 +1,19 @@
 window.addEventListener("DOMContentLoaded", function(){
     const email = document.querySelector("#usermail");
     const passwords = document.querySelector("#passwords");
-    const valide= document.querySelector("#validate");
-    var error = document.querySelector("#error_message")
+    const valide = document.querySelector("#validate");
+    const error = document.querySelector("#error_message");
     const _login1 = "tsioryR";
     const _login2 = "azerty01";
-
-    valide.addEventListener('click',(e) => {
-        e.preventDefault(); /*allow to load my page instead*/
+    
+    valide.addEventListener('click', (e) => {
         if (email.value != _login1 || passwords.value != _login2) {
-            error.style.display = "block";
-            //toggle need to swift
+            error.classList.add("show");
+            setTimeout(() => {
+                error.classList.remove("show");
+            }, 5000);
         } else {
             window.location.href = "index.html";
         }
-    }
-)
+    });    
 });
